1c922db9a70adf6af57d1387d245d90efe04e5dc
webChenGuangXue <1084983891@qq.com>
2020-10-15 12:08:51 +0800

    add

---
+++ client.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+WORK_PATH='/usr/project/clientTest'
+cd $WORK_PATH
+echo "开始清除老代码"
+git reset --hard origin/master
+git clean -f
+echo "开始拉取最新代码"
+git pull origin master
+echo "开始编译代码"
+npm run build
+echo "开始执行构建"
+docker build -t client-test:1.0 .
+echo "停止旧容器并删除"
+docker stop clientTest-container
+docker rm clientTest-container
+echo "启动新容器"
+docker container run -p 80:80 --name clientTest-container -d client-test:1.0
