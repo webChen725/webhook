b9b1de3d9924d4e2abd1906d4c77d1ca69bfedeb
webChenGuangXue <1084983891@qq.com>
2020-10-15 10:52:06 +0800

    add

--- server.sh
+++ server.sh
@@ -7,9 +7,9 @@ git clean -f
 echo "开始拉取最新代码"
 git pull origin master
 echo "开始执行构建"
-docker build -t developmentTest:1.0 .
+docker build -t development-test:1.0 .
 echo "停止旧容器并删除"
 docker stop developmentTest-container
 docker rm developmentTest-container
 echo "启动新容器"
-docker container run -p 3000:3000 --name developmentTest-container -d developmentTest:1.0
+docker container run -p 3000:3000 --name developmentTest-container -d development-test:1.0
